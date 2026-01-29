7dbff72ae836a291d6bc33a60390b5deb54e404a
Yeray <yerayllopes07@gmail.com>
2026-01-29 08:42:51 +0100

    Data Added

--- src/App.java
+++ src/App.java
@@ -1,15 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Fresco f1 = new Fresco("18 Noviembre", 1, "10 Enero", "Spain");
-        Fresco f2 = new Fresco(null, 0, null, null);
-        Refigerado r1 = new Refigerado(null, 0, null, null, 0);
-        Refigerado r2 = new Refigerado(null, 0, null, null, 0);
-        Refigerado r3 = new Refigerado(null, 0, null, null, 0);
-        Agua c2 = new Agua(null, 0, null, null, 0, 0);
-        Agua c3 = new Agua(null, 0, null, null, 0, 0);
-        Aire c4 = new Aire(null, 0, null, null, 0, 0, 0, 0, 0);
-        Aire c5 = new Aire(null, 0, null, null, 0, 0, 0, 0, 0);
-        Nitrogeno c1 = new Nitrogeno(null, 0, null, null, 0, null, 0);
+        Fresco f2 = new Fresco("1 Enero", 2, "5 Marzo", "France");
+        Refigerado r1 = new Refigerado("14 Febrero", 3, "10 Julio", "Mexico", 10);
+        Refigerado r2 = new Refigerado("20 Marzo", 4, "15 Enero", "Italy", 5);
+        Refigerado r3 = new Refigerado("25 Abril", 5, "20 Febrero", "Germany", 8);
+        Agua c2 = new Agua("30 Mayo", 6, "10 Marzo", "Norway", -18, 2.5);
+        Agua c3 = new Agua("15 Junio", 7, "5 Abril", "Iceland", -20, 3.2);
+        Aire c4 = new Aire("10 Julio", 8, "1 Mayo", "Netherlands", -22, 78.0, 21.0, 0.04, 0.96);
+        Aire c5 = new Aire("5 Agosto", 9, "15 Mayo", "Belgium", -25, 80.0, 20.0, 0.03, 0.97);
+        Nitrogeno c1 = new Nitrogeno("20 Septiembre", 10, "1 Junio", "Denmark", -196, "Inmersión directa", 45.5);
 
         System.out.println("*** Empresa Agroalimenticia ***");
         System.out.println(f1);
